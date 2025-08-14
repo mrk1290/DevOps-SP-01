cc97a3433588642dbc546bdb504b39096c4d148b
mcit <mcit@SFO-AMP-TCM-5.lan>
2025-08-14 17:58:01 -0500

    Adding Subs Id

--- variables.tf
+++ variables.tf
@@ -9,3 +9,8 @@ variable "azurerm_rg_location" {
     type = string
     default = "eastus"
 }
+  variable "subscription_id" {
+    description = "Subscription Id"
+    type = string
+    default = "dbd9a6b8-6cb8-4b49-b431-891de1a6f1f7"
+  }
